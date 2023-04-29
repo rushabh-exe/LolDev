54c63e66581eddf3be63f711193128da92a647ee
rushabh <119485580+rushabh-exe@users.noreply.github.com>
2023-04-29 17:38:57 +0530

    Update Footer.js

--- loldev/src/components/Footer.js
+++ loldev/src/components/Footer.js
@@ -1,40 +1,27 @@
-import React from 'react';
+import React from 'react'
 
 function Footer() {
   return (
-<>
-
-<footer className="footer">
-   <div className="footer-content">
-      <div className="footer-logo">
-         <img src="https://picsum.photos/50/50" alt="Footer Logo" />
-         <h3>Vonty</h3>
-      </div>
-      <div className="footer-links">
-         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Contact Us</a></li>
-         </ul>
-         <ul>
-            <li><a href="#">Terms of Use</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">FAQ</a></li>
-         </ul>
-      </div>
-      <div className="footer-social">
-         <a href="#" className="social-icon"><i className="fab fa-facebook-f"></i></a>
-         <a href="#" className="social-icon"><i className="fab fa-twitter"></i></a>
-         <a href="#" className="social-icon"><i className="fab fa-instagram"></i></a>
-         <a href="#" className="social-icon"><i className="fab fa-linkedin-in"></i></a>
-      </div>
-   </div>
-   <div className="footer-bottom">
-      <p>&copy; 2023 Vonty. All rights reserved.</p>
-   </div>
-</footer>
-</>
-  );
+    <div className='futur'>
+        <div className="company-tag">
+        <img src="https://picsum.photos/100/100" className='pfp' alt="1" />
+        <h2 className='hed'>Vonty</h2>
+        </div>
+        <ul className='futul'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>FAQ</li>
+            <li>&copy; 2023 Vonty. All rights reserved.</li>
+        </ul>
+        <div className="social-handles">
+        <img src="https://picsum.photos/100/100" className='pfp' alt="1" />
+        <img src="https://picsum.photos/100/100" className='pfp' alt="1" />
+        <img src="https://picsum.photos/100/100" className='pfp' alt="1" />
+        <img src="https://picsum.photos/100/100" className='pfp' alt="1" />
+        </div>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
