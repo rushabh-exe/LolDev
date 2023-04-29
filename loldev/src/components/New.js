24fc140e1e9458cedf1cdd6079565002318dba91
rushabh <119485580+rushabh-exe@users.noreply.github.com>
2023-04-29 17:40:10 +0530

    Update New.js

--- loldev/src/components/New.js
+++ loldev/src/components/New.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React from 'react'
 
 function New() {
   return (
-    <div>
-      <h1>New Component</h1>
-      <p>Welcome to the New Component!</p>
+    <div className="new-content">
+      <div className="heading">
+        <h2>New</h2>
+      </div>
+      <div className="divider">
+        <hr style={{ width: "100%" }} />
+        <select name="Choose" id="relevant-select">
+          <option value="New">New</option>
+          <option value="Most-Popular">Most-Popular</option>
+          <option value="Most-Vacancy">Most-Vacancy</option>
+        </select>
+      </div>
+       {/*New-main*/}
+       <div className="list-community">
+       <div className="community-card">
+                    <img src="https://picsum.photos/100/100" alt="community Cover Image" />
+                    <div className="community-details">
+                        <h3 className="community-title">community Title</h3>
+                        <p className="community-description">Short description of the community.</p>
+                        <ul className="community-metadata">
+                            <li><i className="fas fa-calendar-alt"></i> July 1 - 7, 2022</li>
+                            <li><i className="fas fa-globe"></i> Offline</li>
+                            <li><i className="fas fa-dollar-sign"></i>paying $500</li>
+                        </ul>
+                        <a href="#" className="community-button">Apply Now</a>
+                    </div>
+                </div>
+       </div>
     </div>
-  );
+  )
 }
 
-export default New;
+export default New
