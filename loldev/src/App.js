bada31e2fef2dc70aae123c93c940b2db6b6777e
rushabh <119485580+rushabh-exe@users.noreply.github.com>
2023-04-29 17:36:15 +0530

    Update App.js

--- loldev/src/App.js
+++ loldev/src/App.js
@@ -1,33 +1,50 @@
-import React, { useState } from 'react';
-import Home from './components/Home';
-import New from './components/New';
-import Static from './components/Static';
-import Dev from './components/Dev';
-import Footer from './components/Footer';
 import './App.css';
+import Home from './components/Home';
 import Profile from './components/Profile';
+import New from './components/New';
+import Dev from './components/Dev';
+import { useState } from 'react';
+import './App.css';
+import Footer from './components/Footer';
 
 function App() {
-  const [activeComponent, setActiveComponent] = useState('home');
+  const [selectedComponent, setSelectedComponent] = useState(<Home />);
 
-  const renderComponent = () => {
-    switch (activeComponent) {
-      case 'home':
-        return <Home />;
-      case 'new':
-        return <New />;
-      case 'dev':
-        return <Dev />;
-      default:
-        return null;
-    }
-  };
+  const handleButtonClick = (component) => {
+    setSelectedComponent(component);
+  }
 
   return (
-    <div>
-      <Static setActiveComponent={setActiveComponent} />
-      <div className="content"><Profile/>{renderComponent()}</div>
-      <Footer/>
+    <div className='main'>
+
+
+      <div className="navbar">
+        <div className="company">
+          <img src="https://picsum.photos/100/100" className='pfp' alt="1" />
+          <h2 className='hed'>Vonty</h2>
+        </div>
+        <div className="all-btn-user">
+          <div className="all-btn">
+            <button className='menu-btn' onClick={() => handleButtonClick(<Home />)}>Home</button>
+            <button className='menu-btn' onClick={() => handleButtonClick(<New />)}>New</button>
+            <button className='menu-btn' onClick={() => handleButtonClick(<Dev />)}>Dev</button>
+          </div>
+          <div className="user">
+            <img src="https://picsum.photos/100/100" className='pfp' alt="1" />
+          </div>
+        </div>
+      </div>
+
+
+      <div className="main-content">
+        <div className="leftcontent"><Profile/></div>
+        <div className="rightcontent">
+          {selectedComponent}
+        </div>
+      </div>
+
+
+      <div className="footer"><Footer/></div>
     </div>
   );
 }
