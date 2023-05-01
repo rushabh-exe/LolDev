b9646b8a2d81c405e1ea3d792acd55073e1bfc51
rushabh <119485580+rushabh-exe@users.noreply.github.com>
2023-05-01 10:43:40 +0530

    Update Profile.js

--- loldev/src/components/Profile.js
+++ loldev/src/components/Profile.js
@@ -1,35 +1,65 @@
-import React from 'react'
+import React from "react";
 
 function Home2() {
+
+  
   return (
     <>
-    {/*<!--main-left-->*/}
-    
+      {/*<!--main-left-->*/}
+
       <div className="left-content">
-     
-        <div className="user-info-box">
-          <img src="https://picsum.photos/200/200" alt="" className="background-image" />
-          <div className="userpfpdiv"><img src="https://picsum.photos/100/100" alt="" className="user-pfp" /></div>
+        <div  className="user-info-box">
+          <img
+            src="https://picsum.photos/200/200"
+            alt=""
+            className="background-image"
+          />
+          <div className="userpfpdiv">
+            <img
+              src="https://picsum.photos/100/100"
+              alt=""
+              className="user-pfp"
+            />
+          </div>
           <h3 className="username-inbox">Username</h3>
-          <p className="user-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi tempore atque nobis, nisi nostrum a nam eum facilis maiores quia.</p>
+          <p className="user-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+            tempore atque nobis, nisi nostrum a nam eum facilis maiores quia.
+          </p>
           <p className="user-email">xyz@gmail.com</p>
           <p className="user-contact">123xxxx321</p>
-          <button className="open-chat-box">Open Chat Box</button>
         </div>
+        
         <div className="chat-box">
-          <header>Chat Assistant</header>
+          <header><h3>Chat Assistant</h3> <button
+              className="cancel-button"
+              onClick={() => {
+                const chatBox = document.querySelector(".chat-box");
+                if (chatBox.classList.contains("show-chat-box")) {
+                  chatBox.classList.add("hide-chat-box");
+                  chatBox.classList.remove("show-chat-box");
+                }
+              }}
+            >
+              X
+            </button></header>
           <div className="chat">
             <p className="bot-message">I am here to help you!</p>
           </div>
           <footer>
             <input type="text" name="" id="user-input-message" />
-            <button className="send-button">OK</button>
-            <button className="cancel-button">Cancel</button>
+            <button className="send-button">҉</button>
           </footer>
         </div>
+        <button
+            className="open-chat-button"
+            onClick={() => {
+              const chatBox = document.querySelector(".chat-box");
+              if (chatBox.classList.contains("show-chat-box")) {chatBox.classList.add("hide-chat-box");chatBox.classList.remove("show-chat-box");} 
+              else {chatBox.classList.add("show-chat-box");chatBox.classList.remove("hide-chat-box");}}}>Bot</button>
       </div>
     </>
-  )
+  );
 }
 
-export default Home2
+export default Home2;
